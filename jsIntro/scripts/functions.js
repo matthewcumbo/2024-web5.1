f74437d41a35fa445a195eccd79e88b94733fc9e
matthewcumbo <matthew.abraham.cumbo@mcast.edu.mt>
2024-12-11 22:18:08 +0100

    Added notes

--- jsIntro/scripts/functions.js
+++ jsIntro/scripts/functions.js
@@ -3,11 +3,18 @@ function helloWorld(){
     console.log('hello world');
 }
 
+// This function requires a value to work
+// It is called a parameter
 function helloUser(user){
     console.log('Hello ' + user);
 }
 
+// There is no limit to the number of parameters a function uses
 function add(num1, num2){
+    /* 
+        Not all functions need to immediately output something
+        We can simply return a value to wherever this function is called
+    */
     return num1 + num2;
 }
 
